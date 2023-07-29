f901aac524fdd978318caad57f120f3e60f39c44
Ismoil-Nigmatov <ismoilnigmatov98@gmail.com>
2023-07-29 14:48:11 +0500

    Adaptation

--- js/index.js
+++ js/index.js
@@ -195,11 +195,13 @@
             }
         }
         else {
-            close(burgerWrap, 'burger__wrap--active', 'burger__wrap--not-active');
-            close(burgerInfoWrap, 'burger__information--active', 'burger__information--not-active');
-            close(orderHistory, 'order__history--active', 'order__history--not-active');
             close(orderWrap, 'order__wrap--active', 'order__wrap--not-active');
+            close(burgerInfoWrap, 'burger__information--active', 'burger__information--not-active');
+            close(burgerWrap, 'burger__wrap--active', 'burger__wrap--not-active');
             close(accountWrap, 'account__wrap--active', 'account__wrap--not-active');
+            close(orderHistory, 'order__history--active', 'order__history--not-active');
+            close(dish , 'dish__wrap--active' , 'dish__wrap--not-active');
+            close(payment, 'payment__wrap--active', 'payment__wrap--not-active');
         }
     }
 
